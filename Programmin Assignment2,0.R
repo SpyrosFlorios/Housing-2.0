aeea29b6db72107969704352a1df59c5926d60db
SpyrosFlorios <spyrosf199@gmail.com>
2025-06-12 17:54:51 +0300

    Cleaned up the Code

--- Programmin Assignment2,0.R
+++ Programmin Assignment2,0.R
@@ -1,12 +1,20 @@
-usethis::use_git()
-data <- read.csv("~/Programming.csv")
-data2 <- read.csv("~/Programming2.csv")
+#Reading the CBS Data
+data <- read.csv("~/GitHub/Housing-2.0/Programming.csv")
+data2 <- read.csv("~/GitHub/Housing-2.0/Programming2.csv")
 summary(data)
 summary(data2)
+
+#Installing Packages
+install.packages("sf")
+install.packages("cbsodataR")
 install.packages("tidyverse")
 library(tidyr)
 library(dplyr)
+library(ggplot2)
+library(cbsodataR)
+library(sf)
 
+#Data Cleaning
 Provence_blocks <- list()
 
 for (i in seq(1, ncol(data), by = 4)) {
@@ -16,6 +24,8 @@ for (i in seq(1, ncol(data), by = 4)) {
 }
 
 data2$Provence[data2$Provence == "Friesland"] <- "Fryslân"
+
+data2$Total.Houses <- as.numeric(gsub("\\.","", data2$Total.Houses))
 
 data_long3 <- bind_rows(Provence_blocks)
 
@@ -36,10 +46,8 @@ data_long3 <- data_long3 %>%
 data_long3 <- data_long3 %>%
   mutate(Percent_growth = (House_growth / lag(total_houses)) * 100)
 
-data2$Total.Houses <- as.numeric(gsub("\\.","", data2$Total.Houses))
 
-library(ggplot2)
-
+#Plotting a Line Graph
 ggplot(data_long3, aes(x = period, y = House_growth, color = Provence)) +
   geom_line(size = 0.5) +
   labs(
@@ -50,29 +58,12 @@ ggplot(data_long3, aes(x = period, y = House_growth, color = Provence)) +
   ) +
   theme_minimal()
 
-library("cbsodataR")
-install.packages("cbsodataR")
-cbs_maps <- cbs_get_maps()
-install.packages("cbs_get_maps")
-str(cbs_maps$region)
-library("sf")
-provincie_2023 <- cbs_get_sf("provincie", 2023)
-install.packages("sf")
-plot(provincie_2023, max.plot = 1)
-install.packages("cbs_get_sf")
-
-library(cbsodataR)
-library(sf)
-library(dplyr)
-library(ggplot2)
-
-
+#Plotting the Dutch Provence Map
 prov_map <- cbs_get_sf(region = "provincie", year = 2023)
 
 future_data <- data2 |>
   rename(statnaam = Provence,   
          supply_value = Total.Houses)   
-
 
 future_map <- left_join(prov_map, future_data, by = "statnaam")
 
@@ -80,4 +71,3 @@ ggplot(future_map) +
   geom_sf(aes(fill = supply_value)) +
   labs(title = "Housing Supply (future 7 Years)", fill = "supply") + 
   theme_void()
-
